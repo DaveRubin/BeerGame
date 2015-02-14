816e746cdb9ed4d2912f6bf1986021c5741d5b0d
Dudi Rubin <dudrubin@gmail.com>
2015-02-14 09:50:26 +0200

    capsulated function into user selection directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,19 +5,8 @@ angular.module('beerGameApp')
 
         $scope.players = [new Player()];
 
-        $scope.addPlayer = function(){
-          $scope.players.push(new Player());
-        };
 
-        $scope.toggleEdit = function(selectedPlayer){
-            for (var i = 0; i < $scope.players.length; i++) {
-                var player = $scope.players[i];
-                if (selectedPlayer == player){
-                    player.edit = !player.edit;
-                }
-                else player.edit = false;
-            }
-        };
+
 
         $scope.toggleSidebar = function () {
             console.log("toggle");
--- app/scripts/directives/user-selection.js
+++ app/scripts/directives/user-selection.js
@@ -1,12 +1,30 @@
 'use strict';
 
 angular.module('beerGameApp')
-    .directive('userSelection', function () {
+    .directive('userSelection', function (Player) {
         return {
             templateUrl: "views/user-selection.html",
             restrict: 'E',
-            link: function postLink(scope, element, attrs) {
+            link: function postLink(scope) {
+                scope.startGame = function(){
+                    console.log("A");
+                };
 
+                scope.addPlayer = function(){
+                  scope.players.push(new Player());
+                };
+
+                scope.toggleEdit = function(selectedPlayer){
+
+                    for (var i = 0; i < scope.players.length; i++) {
+                        var player = scope.players[i];
+
+                        if (selectedPlayer == player){
+                            player.edit = !player.edit;
+                        }
+                        else player.edit = false;
+                    }
+                };
             }
         };
     });
