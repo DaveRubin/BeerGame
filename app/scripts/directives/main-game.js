9227e484612d782be46a4e96935a1f3e5ade5366
Dudi Rubin <dudrubin@gmail.com>
2015-02-25 22:14:48 +0200

    fan animation

--- app/scripts/directives/main-game.js
+++ app/scripts/directives/main-game.js
@@ -1,17 +1,25 @@
 'use strict';
 
 angular.module('beerGameApp')
-    .directive('mainGame', function () {
+    .directive('mainGame', function ($timeout) {
         return {
             templateUrl: 'views/main-game.html',
             restrict: 'E',
             link: function postLink(scope, element, attrs) {
+                var FAN_DURATION = 1250;
                 scope.currentPlayer = 0;
                 scope.nextPlayer = function(){
                     scope.currentPlayer++;
                     if (scope.currentPlayer == scope.players.length){
                         scope.currentPlayer -= scope.players.length;
                     }
+                };
+                scope.startFan = function(){
+                    scope.fanAnimation = false;
+                    scope.fanAnimation = true;
+                    $timeout(function(){
+                        scope.fanAnimation = false;
+                    },FAN_DURATION)
                 }
             }
         };
